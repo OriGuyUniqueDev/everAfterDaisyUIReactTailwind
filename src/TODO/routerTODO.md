6f37b158e196bdd67968e8200905a95b7f161ad2
Ori Guy <origuydev@gmail.com>
2023-06-06 06:06:44 +0300

    add TODO for the router

---
+++ src/TODO/routerTODO.md
@@ -0,0 +1,21 @@
+# EverAfter - Frontend Tasks
+
+## Router Structure
+
+---
+
+## **[]** Client side routing - URL
+
+---
+
+    [v] Branch: configure router
+
+---
+
+**[]** **Tasks:**
+
+**[]** "/" - main landing page route\
+**[?]** "/login - route for login screen\
+**[]** "/register" - route for the register component\
+**[]** "/features" - jump to features section
+**[]** "about" - link to about page
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routerEndPoints from "./router/routerEndPoints";
+import Layout from "./layout/Layout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={routerEndPoints}></RouterProvider>
+		<Layout>
+			<RouterProvider router={routerEndPoints}></RouterProvider>
+		</Layout>
 	</React.StrictMode>
 );
